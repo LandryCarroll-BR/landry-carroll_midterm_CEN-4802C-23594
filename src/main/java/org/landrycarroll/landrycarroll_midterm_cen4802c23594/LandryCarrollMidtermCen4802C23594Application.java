b6860e4a1717863fda07c4805eda03c875ca956f
LandryCarroll-BR <carroll.landry@gmail.com>
2026-02-28 19:45:54 -0500

    initial commit

---
+++ src/main/java/org/landrycarroll/landrycarroll_midterm_cen4802c23594/LandryCarrollMidtermCen4802C23594Application.java
@@ -0,0 +1,13 @@
+package org.landrycarroll.landrycarroll_midterm_cen4802c23594;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LandryCarrollMidtermCen4802C23594Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LandryCarrollMidtermCen4802C23594Application.class, args);
+    }
+
+}
---
+++ src/test/java/org/landrycarroll/landrycarroll_midterm_cen4802c23594/LandryCarrollMidtermCen4802C23594ApplicationTests.java
@@ -0,0 +1,13 @@
+package org.landrycarroll.landrycarroll_midterm_cen4802c23594;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LandryCarrollMidtermCen4802C23594ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
